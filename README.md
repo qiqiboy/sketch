4c9e2290787352176ec067c7b2f2c3ac07b61dfe
qiqiboy <imqiqiboy@gmail.com>
2014-12-24 17:59:35 +0800

    add file

---
+++ README.md
@@ -0,0 +1,50 @@
+Sketch
+=====
+
+Just a HTML5 sketch-pad. 
+简易H5画板，支持绘画与擦除
+
+## 如何使用
+```javascript
+
+// @Class Sketch
+// @param String|HTMLCanvasElement id canvas节点id或者该对象
+// @param Object config 初始画板配置
+                        .width int 画板宽度
+                        .height int 画板高度
+                        .lineWidth int 笔画宽度
+                        .color 笔画颜色
+
+var sketch=new Sketch('canvas',{
+    width:document.body.clientWidth,
+    height:document.body.clientHeight,
+    lineWidth:5,
+    color:'red'
+});
+
+// @event 
+// start 开始绘画
+// move 绘画中
+// end 绘画结束
+
+sketch.on('start',function(){
+	console.log('开始绘画');
+});
+sketch.on('move',function(){
+	console.log('绘画中');
+});
+sketch.on('end',function(){
+	console.log('绘画结束');
+});
+
+// @method
+sketch.clear(); //清除画布重新开始
+mask.cancel(num); //上一步，num为取消步数，默认为1
+
+// erase擦除
+sketch.erase=true; //该属性为true时表示是擦除状态
+
+````
+
+## DEMO 
+http://u.boy.im/sketch
---
+++ src/sketch.js
@@ -0,0 +1,186 @@
+/*
+ * @author qiqiboy
+ * @github https://github.com/qiqiboy/sketch
+ */
+;
+(function(ROOT, struct, undefined){
+    "use strict";
+
+    if(typeof Function.prototype.bind!='function'){
+        Function.prototype.bind=function(obj){
+            var self=this;
+            return function(){
+                return self.apply(obj,arguments);
+            }
+        }
+    }
+
+    var evstr='PointerEvent' in ROOT ?
+            "pointerdown pointermove pointerup pointercancel" :
+            "createTouch" in ROOT.document || 'ontouchstart' in ROOT ?
+            "touchstart touchmove touchend touchcancel" :
+            "mousedown mousemove mouseup";
+    
+    struct.prototype={
+        constructor:struct,
+        lineWidth:5,
+        color:'#000',
+        erase:false,
+        init:function(config){
+            this.events={};
+            this.actions=[];
+
+            this.ctx=this.canvas.getContext('2d');
+
+            typeof config=='object' && "width height lineWidth color".split(" ").forEach(function(prop){
+                this[prop]=typeof config[prop]=='undefined'?this[prop]:config[prop];
+            }.bind(this));
+
+            evstr.split(" ").forEach(function(ev){
+                this.canvas.addEventListener(ev, this, false);
+            }.bind(this));
+            
+            this.on({
+                start:function(){
+                   this.actions.push({
+                       width:this.lineWidth,
+                       color:this.color,
+                       erase:this.erase,
+                       pens:[arguments]
+                   });
+                },
+                move:function(){
+                    this.actions[this.steps-1].pens.push(arguments);
+                    this.draw();
+                }
+            });
+        },
+        handleEvent:function(ev){
+            var x=ev.clientX||0,
+                y=ev.clientY||0,
+                rect=this.canvas.getBoundingClientRect();
+            if(ev.touches && ev.touches.length){
+                if(ev.touches.length>1){//多指触摸不作响应
+                    return;
+                }
+                x=ev.touches.item(0).clientX;
+                y=ev.touches.item(0).clientY;
+            }
+            x-=rect.left;
+            y-=rect.top;
+
+            x*=this.width/rect.width;
+            y*=this.height/rect.height;
+
+            switch(ev.type.toLowerCase()){
+                case 'mousedown':
+                case 'touchstart':
+                case 'pointerdown':
+                    this.moving=true;
+                    this.fire('start',x,y);
+                    break;
+                case 'mousemove':
+                case 'touchmove':
+                case 'pointermove':
+                    if(this.moving){
+                        ev.preventDefault();
+                        this.fire('move',x,y);
+                    }
+                    break;
+                case 'mouseup':
+                case 'touchend':
+                case 'touchcancel':
+                case 'pointerup':
+                case 'pointercancel':
+                    if(this.moving){
+                        delete this.moving;
+                        this.fire('end');
+                    }
+                    break;
+            }
+        },
+        on:function(ev,callback){
+            if(typeof ev == 'object'){
+                return Object.keys(ev).forEach(function(_e){
+                    this.on(_e,ev[_e]);
+                }.bind(this));
+            }
+            if(!this.events[ev]){
+                this.events[ev]=[];
+            }
+            this.events[ev].push(callback);
+        },
+        fire:function(ev){
+            var args=[].slice.call(arguments,1);
+            (this.events[ev]||[]).forEach(function(callback){
+                if(typeof callback == 'function'){
+                    callback.apply(this,args);
+                }
+            }.bind(this));
+        },
+        draw:function(){
+            var ctx=this.ctx;
+            ctx.clearRect(0,0,this.width,this.height);
+            this.actions.forEach(function(action){
+                ctx.lineWidth=action.width;
+                ctx.lineJoin=ctx.lineCap='round';
+                if(action.erase){
+                    ctx.strokeStyle="#000";
+                    ctx.globalCompositeOperation='destination-out';
+                }else{
+                    ctx.globalCompositeOperation='source-over';
+                    ctx.strokeStyle=action.color;
+                }
+                ctx.beginPath();
+                action.pens.forEach(function(pos,step){
+                    ctx[step?'lineTo':'moveTo'].apply(ctx,pos);
+                });
+                ctx.stroke();
+                ctx.closePath();
+            });
+            return this;
+        },
+        cancel:function(num){
+            this.actions.splice(-(num||1));
+            return this.draw();
+        },
+        clear:function(x,y,w){
+            this.actions.length=0;
+            return this.draw();
+        }
+    }
+
+    if(typeof Object.defineProperties=='function'){
+        
+        "width height".split(" ").forEach(function(prop){
+            Object.defineProperty(struct.prototype,prop,{
+                get:function(){
+                    return this.canvas[prop];
+                },
+                set:function(value){
+                    this.canvas[prop]=value;
+                },
+                enumerable:true
+            });
+        });
+
+        Object.defineProperty(struct.prototype,'steps',{
+            get:function(){
+                return this.actions.length;
+            },
+            enumerable:true
+        });
+
+    }
+
+    ROOT.Sketch=struct;
+    
+})(window, function(id,config){
+    if(!(this instanceof arguments.callee)){
+        return new arguments.callee(id,config);
+    }
+
+    this.canvas=typeof id=='string'?document.getElementById(id):id;
+
+    this.init(config);
+});
